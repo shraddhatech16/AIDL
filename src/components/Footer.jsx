660c732ef5b39bb69b17a3e68a4810126972786b
shraddhatech16 <rajendraalawane@gmail.com>
2023-04-30 15:49:04 +0530

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       id:1,
       child:(
           <>
-          LinkedIn<FaLinkedin size={30}/>
+          <FaLinkedin size={30}/>
           </>
       ),
       href:'https://www.linkedin.com/in/fcrit-aidl-group-2948b5228/',
@@ -21,7 +21,7 @@ const Footer = () => {
       id:2,
       child:(
           <>
-          Instagram<AiFillInstagram size={30}/>
+          <AiFillInstagram size={30}/>
           </>
       ),
       href:'https://www.instagram.com/aidl_fcrit/?igshid=YmMyMTA2M2Y%3D',
@@ -30,7 +30,7 @@ const Footer = () => {
       id:3,
       child:(
           <>
-          Mail<HiOutlineMail size={30}/>
+          <HiOutlineMail size={30}/>
           </>
       ),
       href:'mailto: aidl@gmail.com',
@@ -39,7 +39,7 @@ const Footer = () => {
       id:4,
       child:(
           <>
-          Become a Member<BsFillPersonCheckFill size={30}/>
+          <BsFillPersonCheckFill size={30}/>
           </>
       ),
       href : "https://docs.google.com/forms/d/e/1FAIpQLSeIGFlrTR6lVYbN8mm6we1dbX_G6MFC2zHCvp1JcAfG5u3h5A/viewform",
@@ -48,8 +48,23 @@ const Footer = () => {
 
   return (
     <footer className="w-full p-8  bg-gradient-to-b from-black via-black to-gray-800 ">
-      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
-       
+      <div className="flex flex-col flex-nowrap items-center justify-center gap-y-6 gap-x-12 text-center justify-center">
+      <Typography color="blue-gray" className="text-center font-normal text-white flex justify-center">Lets Stay Connected</Typography>
+      <ul>
+      {links.map(({id,child,href, style,download})=>(
+          <li key={id} className={"flex justify-center items-center w-40 h-14 px-4  bg-gray-500" + style}>
+          <a 
+          href={href} 
+          className="flex justify-center items-center w-full text-white" 
+          download={download}
+          target='_blank'
+          rel="noreferrer"
+          >
+          {child}</a>
+          </li>
+  
+      ))}
+      </ul>
       </div>
       <hr className="my-8 border-blue-gray-50" />
       <Typography color="blue-gray" className="text-center font-normal text-white">
