5c091995d6d2a452c0bdf7d00c461c7534d36b9c
shraddhatech16 <rajendraalawane@gmail.com>
2023-05-02 14:24:39 +0530

    corecomm

--- src/components/CoreComm.jsx
+++ src/components/CoreComm.jsx
@@ -137,7 +137,7 @@ return(
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 className="font-dmserif text-3xl font-bold text-white">Assi. Technical Head</h1>
-          <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">MD Tahir</p>
+          <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Md Tahir</p>
           <h1 className="font-dmserif text-2xl font-bold text-white">Lets Connect</h1>
           <button className="rounded-full bg-blue-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60"><a className="text-white" href="https://www.linkedin.com/in/md-tahir-s-91b62422b/">LinkedIn</a></button>
           <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
